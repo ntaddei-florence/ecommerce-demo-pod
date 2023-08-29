98bd59fb50b1c083b7227b6fc55fd92be56a3d16
fabiofognani <ffognani@florence-consulting.it>
2023-08-29 09:53:54 +0200

    feat: size and color selection

--- src/app/products/product-detail.tsx
+++ src/app/products/product-detail.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { redirect } from "next/navigation";
-import { FC, useMemo, useState } from "react";
+import { useRouter } from "next/navigation";
+import { FC, useCallback, useMemo } from "react";
 
 import { MediaCarousel } from "~/components/media-carousel";
 import { ProductDetailDataFragment, VariantDataFragment } from "~/graphql/generated/graphql";
@@ -14,11 +14,6 @@ export interface ProductDetailProps {
 export const ProductDetail: FC<ProductDetailProps> = ({ product, variant }) => {
   const media = variant.media ?? product.defaultMedia;
 
-  const redirectToVariant = (v: VariantDataFragment) => {
-    if (v.slug) redirect(`/products/${v.slug}`);
-    else redirect(`/products/${product.slug}/sku/${v.sku}`);
-  };
-
   const allVariants = product.variantsCollection?.items;
   const availableColors = useMemo(() => {
     return Array.from(new Set(allVariants?.map((v) => v?.color)));
@@ -27,8 +22,51 @@ export const ProductDetail: FC<ProductDetailProps> = ({ product, variant }) => {
     return Array.from(new Set(allVariants?.map((v) => v?.size?.label)));
   }, [allVariants]);
 
-  const [selectedColor, setSelectedColor] = useState(variant.color);
-  const [selectedSize, setSelectedSize] = useState(variant.size?.label);
+  const router = useRouter();
+
+  const redirectToVariant = useCallback(
+    (v: VariantDataFragment) => {
+      if (v.slug) router.push(`/products/${v.slug}`);
+      else router.push(`/products/${product.slug}/sku/${v.sku}`);
+    },
+    [product, router]
+  );
+
+  const selectVariantForColor = useCallback(
+    (color: string) => {
+      const variantsForColor = allVariants?.filter((v) => color === v?.color);
+      const variantSameSize = variantsForColor?.find(
+        (v) => v?.size?.label === variant?.size?.label
+      );
+      const variantForColor = variantSameSize ?? variantsForColor?.[0];
+      if (variantForColor) {
+        redirectToVariant(variantForColor);
+      }
+    },
+    [variant, allVariants, redirectToVariant]
+  );
+
+  const selectVariantForSize = useCallback(
+    (size: string) => {
+      const variantForSize = allVariants?.find(
+        (v) => v?.color === variant.color && size === v?.size?.label
+      );
+      if (variantForSize) {
+        redirectToVariant(variantForSize);
+      }
+    },
+    [allVariants, redirectToVariant, variant]
+  );
+
+  const isSizeVariantAvailableForColor = useCallback(
+    (size: string) => {
+      return (
+        (allVariants?.filter((v) => v?.color === variant.color && size === v?.size?.label) ?? [])
+          .length > 0
+      );
+    },
+    [allVariants, variant]
+  );
 
   return (
     <div className="container mx-auto">
@@ -40,20 +78,36 @@ export const ProductDetail: FC<ProductDetailProps> = ({ product, variant }) => {
         )}
         <div className="prose pb-4">
           <h2>{product?.name}</h2>
-          <h3>Size: {variant?.size?.label}</h3>
           <h3>
             Colors:{" "}
             {availableColors.map((color) => (
               <button
-                key={variant.sku}
-                title={variant.sku ?? ""}
-                // onClick={() => v && redirectToVariant(v)}
+                key={color}
+                title={color ?? undefined}
+                onClick={() => color && selectVariantForColor(color)}
                 style={{ backgroundColor: color ?? undefined }}
                 className={`mx-1 rounded-md border border-4 w-6 h-6 ${
                   variant?.color === color ? "border-accent" : ""
                 }`}
               />
             ))}
+          </h3>
+          <h3>
+            Size:
+            <select
+              className="select select-accent inline ml-4 max-w-xs"
+              onChange={(e) => selectVariantForSize(e.target.value)}
+            >
+              {availableSizes.filter(Boolean).map((size) => (
+                <option
+                  key={size}
+                  disabled={!isSizeVariantAvailableForColor(size!)}
+                  selected={variant.size?.label === size}
+                >
+                  {size}
+                </option>
+              ))}
+            </select>
           </h3>
           <p>
             SKU: <strong>{variant.sku}</strong>
