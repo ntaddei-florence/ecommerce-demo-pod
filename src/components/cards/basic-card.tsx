eb80444379916617b06097d47ddc63b17579e5f7
fabiofognani <ffognani@florence-consulting.it>
2023-11-15 16:23:46 +0100

    style: fix

--- src/components/cards/basic-card.tsx
+++ src/components/cards/basic-card.tsx
@@ -14,7 +14,7 @@ export const BasicCard: FC<BasicCardProps> = ({ image, title, body, actions }) =
       {image && (
         <Image src={image} className="aspect-square object-cover" alt="" width={512} height={512} />
       )}
-      <div className="card-body mt-4">
+      <div className="card-body">
         <h2 className="card-title">{title}</h2>
         {body}
         {actions && <div className="card-actions justify-end">{actions}</div>}
